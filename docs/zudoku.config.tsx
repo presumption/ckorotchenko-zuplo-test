91482ad801e5a13fe78cdc3f1c5c644b17cdc703
presumption <ckorotchenko@squarespace.com>
2025-08-13 16:08:22 +0000

    Initial commit from Zuplo connect

---
+++ docs/zudoku.config.tsx
@@ -0,0 +1,113 @@
+import type { ZudokuConfig } from "zudoku";
+
+/**
+ * Developer Portal Configuration
+ * For more information, see:
+ * https://zuplo.com/docs/dev-portal/zudoku/configuration/overview
+ */
+const config: ZudokuConfig = {
+  site: {
+    title: "My Developer Portal",
+    logo: {
+      src: {
+        light: "https://cdn.zuplo.com/assets/my-dev-portal-light.svg",
+        dark: "https://cdn.zuplo.com/assets/my-dev-portal-dark.svg",
+      },
+    },
+  },
+  metadata: {
+    title: "Developer Portal",
+    description: "Developer Portal",
+  },
+  navigation: [
+    {
+      type: "category",
+      label: "Documentation",
+      items: [
+        {
+          type: "category",
+          label: "Getting Started",
+          icon: "sparkles",
+          items: [
+            {
+              type: "doc",
+              file: "introduction",
+            },
+            {
+              type: "doc",
+              file: "markdown",
+            },
+          ],
+        },
+        {
+          type: "category",
+          label: "Useful Links",
+          collapsible: false,
+          icon: "link",
+          items: [
+            {
+              type: "link",
+              label: "Zuplo Docs",
+              to: "https://zuplo.com/docs/dev-portal/introduction",
+            },
+            {
+              type: "link",
+              label: "Developer Portal Docs",
+              to: "https://zuplo.com/docs/dev-portal/introduction",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      type: "link",
+      to: "/api",
+      label: "API 1",
+    },
+    {
+      type: "link",
+      to: "/api2",
+      label: "API 2",
+    },
+    {
+      type: "link",
+      to: "/books_api",
+      label: "Books API",
+    },
+  ],
+  redirects: [{ from: "/", to: "/api" }],
+  apis: [
+    {
+      type: "file",
+      input: "../config/routes.oas.json",
+      path: "api",
+    },
+    {
+      type: "file",
+      input: "../config/routes2.oas.json",
+      path: "api2",
+    },
+    {
+      type: "file",
+      input: "../config/books-spec.oas.json",
+      path: "books_api",
+    },
+  ],
+  authentication: {
+    // IMPORTANT: This is a demo Auth0 configuration.
+    // In a real application, you should replace these values with your own
+    // identity provider's configuration.
+    // This configuration WILL NOT WORK with custom domains.
+    // For more information, see:
+    // https://zuplo.com/docs/dev-portal/zudoku/configuration/authentication
+    type: "auth0",
+    domain: "auth.zuplo.site",
+    clientId: "f8I87rdsCRo4nU2FHf0fHVwA9P7xi7Ml",
+    audience: "https://api.example.com/",
+  },
+  apiKeys: {
+    enabled: true,
+  },
+};
+
+export default config;
